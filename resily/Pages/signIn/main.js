fedefa7d8af7bde2fa12f0e7fa5181acfc59a227
ResilyDemoDay <150312958+ResilyDemoDay@users.noreply.github.com>
2023-11-30 01:55:51 -0300

    Add files via upload

---
+++ resily/Pages/signIn/main.js
@@ -0,0 +1,10 @@
+// Referência ao botão de cadastro
+var botaoCadastrar = document.getElementById("botaoCadastrar");
+
+// Função para redirecionar o usuário quando o botão for clicado
+botaoCadastrar.addEventListener("click", function() {
+    // Redirecionar para a página desejada (substitua "pagina-de-destino.html" pela URL da página para a qual você deseja redirecionar)
+    window.location.href = "pagina-de-destino.html";
+});
+
+
